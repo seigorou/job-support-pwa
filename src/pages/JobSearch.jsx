b5a2b5c69da167ea85588c1d0e749f4193c1b493
seigorou <seita.nishida@gmail.com>
2026-08-10 03:22:17 +0900

    必要機能の配置
    実装が必要な機能の配置順序が分かるように配置した。

--- src/pages/JobSearch.jsx
+++ src/pages/JobSearch.jsx
@@ -1,4 +1,21 @@
 function JobSearch() {
-  return <h1>職種探索</h1>;
+  return (
+    <main>
+      <header>
+        <h1>職種探索</h1>
+      </header>
+
+      <section>
+        <h2>興味のあるタグを選択</h2>
+        <p>ここにタグを置く予定</p>
+      </section>
+      <button>この条件で探す</button>
+
+      <section>
+        <h2>おすすめの職種</h2>
+        <p>ここに職種カードを置く予定</p>
+      </section>
+    </main>
+  );
 }
 export default JobSearch;

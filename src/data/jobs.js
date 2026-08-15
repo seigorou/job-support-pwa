269dc61368a2631a10d1c103ce9f8dea7038294d
seigorou <seita.nishida@gmail.com>
2026-08-15 23:43:02 +0900

    骨組み
    職種探索画面の骨組みをつくった
    細部の調整はできてない

---
+++ src/data/jobs.js
@@ -0,0 +1,33 @@
+import { DessertIcon } from "lucide-react";
+
+const tags = [
+  {
+    id: 1,
+    name: "システムエンジニア",
+    tags: ["プログラミング", "ものづくり"],
+    description: "システムの設計や開発を行う仕事です",
+  },
+
+  {
+    id: 2,
+    name: "データサイエンティスト",
+    tags: ["データ分析", "プログラミング"],
+    description: "データを分析し、課題の発見や意思決定を支援する仕事です",
+  },
+
+  {
+    id: 3,
+    name: "プロジェクトマネージャー",
+    tags: ["デザイン", "企画・マーケ"],
+    description: "webサイトなどのデザインを行う仕事です",
+  },
+
+  {
+    id: 4,
+    name: "プログラマー",
+    tags: ["プログラミング", "ものづくり"],
+    description: "プログラミングをする",
+  },
+];
+
+export default jobs;
--- src/pages/JobSearch.jsx
+++ src/pages/JobSearch.jsx
@@ -1,19 +1,47 @@
+import { useState } from "react";
+import { Search } from "lucide-react";
+import "../styles/JobSearch.css";
+import tags from "../data/tags";
+import jobs from "../data/jobs";
+
 function JobSearch() {
   return (
     <main>
-      <header>
+      <header className="JobSearch-header">
         <h1>職種探索</h1>
+
+        <button className="JobSearch-button">
+          <Search size={22} />
+        </button>
       </header>
 
-      <section>
+      <section className="tag-section">
         <h2>興味のあるタグを選択</h2>
-        <p>ここにタグを置く予定</p>
-      </section>
-      <button>この条件で探す</button>
 
-      <section>
+        <div className="tag-list">
+          {tags.map((tag) => (
+            <button key={tag.id}>{tag.name}</button>
+          ))}
+        </div>
+      </section>
+      <button className="search-condition-button">この条件で探す</button>
+
+      <section className="jobs-section">
         <h2>おすすめの職種</h2>
-        <p>ここに職種カードを置く予定</p>
+        <div className="job-list">
+          {jobs.map((job) => (
+            <article className="job-card" key={job.id}>
+              <h3>{job.name}</h3>
+              <div className="job-tags">
+                {PiFileJpgBold.tags.map((tag) => (
+                  <span key={tag}>{tag}</span>
+                ))}
+              </div>
+
+              <p>{job.description}</p>
+            </article>
+          ))}
+        </div>
       </section>
     </main>
   );
